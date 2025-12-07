8082948c73765b4cf1b4a007cd9876c632b0bd6d
himanshubharti19 <himanshudutt2519@gmail.com>
2025-12-07 18:38:45 -0500

    Create installation.bash

---
+++ installation.bash
@@ -0,0 +1,7 @@
+git clone https://github.com/<your-username>/ML-DL-Myocardial-Infarction-Severity.git
+cd ML-DL-Myocardial-Infarction-Severity
+
+python -m venv venv
+source venv/bin/activate   # on Windows: venv\Scripts\activate
+
+pip install -r requirements.txt
